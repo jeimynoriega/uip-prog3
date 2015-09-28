cf171a7ba84422b1ac3ef00ee09ab855987ff553
jeimynoriega <ojos-lindos29@hotmail.com>
2015-09-28 14:12:59 -0500

    Create quiz.py

---
+++ Laboratorios/quiz.py
@@ -0,0 +1,22 @@
+
+base=5
+altura=7
+
+perimetro=2*5+2*7
+print ("mi perimetro es" + str(perimetro))
+
+area=5*7
+print ("mi area es" + str (area))
+
+metrop=perimetro/100
+print ("mi perimetro en metro es" + str(metrop))
+
+pulgadap=perimetro/2.54
+print ("mi perimetro en pulgada es" + str(pulgadap))
+
+metroa=area/100
+print ("mi area en metro es" + str(metroa))
+
+pulgadaa=area/2.54
+print ("mi area en pulgada es" + str(pulgadaa))
+
